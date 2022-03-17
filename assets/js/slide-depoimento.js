84d7a677d03d7d8c92ee97edf08470d4512fa2d8
cesarpaiva <92410539+cesarpaiva@users.noreply.github.com>
2022-03-17 19:01:56 -0300

    Add files via upload

--- assets/js/slide-depoimento.js
+++ assets/js/slide-depoimento.js
@@ -1,84 +1,40 @@
-jQuery(document).ready(function ($) {
-	var feedbackSlider = $(".feedback-slider");
-	feedbackSlider.owlCarousel({
-		items: 1,
-		nav: true,
-		dots: true,
-		autoplay: true,
-		loop: true,
-		mouseDrag: true,
-		touchDrag: true,
-		navText: [
-			"<i class='fa fa-long-arrow-left'></i>",
-			"<i class='fa fa-long-arrow-right'></i>"
-		],
-		responsive: {
-			// breakpoint from 767 up
-			767: {
-				nav: true,
-				dots: false
-			}
-		}
-	});
 
-	feedbackSlider.on("translate.owl.carousel", function () {
-		$(".feedback-slider-item h3")
-			.removeClass("animated fadeIn")
-			.css("opacity", "0");
-		$(".feedback-slider-item img, .feedback-slider-thumb img, .customer-rating")
-			.removeClass("animated zoomIn")
-			.css("opacity", "0");
-	});
+const slider = document.querySelector(".items");
+const slides = document.querySelectorAll(".item");
+const button = document.querySelectorAll(".button");
 
-	feedbackSlider.on("translated.owl.carousel", function () {
-		$(".feedback-slider-item h3").addClass("animated fadeIn").css("opacity", "1");
-		$(".feedback-slider-item img, .feedback-slider-thumb img, .customer-rating")
-			.addClass("animated zoomIn")
-			.css("opacity", "1");
-	});
-	feedbackSlider.on("changed.owl.carousel", function (property) {
-		var current = property.item.index;
-		var prevThumb = $(property.target)
-			.find(".owl-item")
-			.eq(current)
-			.prev()
-			.find("img")
-			.attr("src");
-		var nextThumb = $(property.target)
-			.find(".owl-item")
-			.eq(current)
-			.next()
-			.find("img")
-			.attr("src");
-		var prevRating = $(property.target)
-			.find(".owl-item")
-			.eq(current)
-			.prev()
-			.find("span")
-			.attr("data-rating");
-		var nextRating = $(property.target)
-			.find(".owl-item")
-			.eq(current)
-			.next()
-			.find("span")
-			.attr("data-rating");
-		$(".thumb-prev").find("img").attr("src", prevThumb);
-		$(".thumb-next").find("img").attr("src", nextThumb);
-		$(".thumb-prev")
-			.find("span")
-			.next()
-			.html(prevRating + '<i class="fa fa-star"></i>');
-		$(".thumb-next")
-			.find("span")
-			.next()
-			.html(nextRating + '<i class="fa fa-star"></i>');
-	});
-	$(".thumb-next").on("click", function () {
-		feedbackSlider.trigger("next.owl.carousel", [300]);
-		return false;
-	});
-	$(".thumb-prev").on("click", function () {
-		feedbackSlider.trigger("prev.owl.carousel", [300]);
-		return false;
-	});
-}); 
+let current = 0;
+let prev = 4;
+let next = 1;
+
+for (let i = 0; i < button.length; i++) {
+  button[i].addEventListener("click", () => i == 0 ? gotoPrev() : gotoNext());
+}
+
+const gotoPrev = () => current > 0 ? gotoNum(current - 1) : gotoNum(slides.length - 1);
+
+const gotoNext = () => current < 4 ? gotoNum(current + 1) : gotoNum(0);
+
+const gotoNum = number => {
+  current = number;
+  prev = current - 1;
+  next = current + 1;
+
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].classList.remove("active");
+    slides[i].classList.remove("prev");
+    slides[i].classList.remove("next");
+  }
+
+  if (next == 5) {
+    next = 0;
+  }
+
+  if (prev == -1) {
+    prev = 4;
+  }
+
+  slides[current].classList.add("active");
+  slides[prev].classList.add("prev");
+  slides[next].classList.add("next");
+}
